99ac438aa56c828b6be174e7da51cd74c78c148f
Chris Small <metasoarous@gmail.com>
2011-07-10 16:00:21 -0700

    Made things a little bit cleaner on the member index. Also made a helper method for helping debug encoding errors in big index results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
 		confirm = options.fetch(:confirm, "Are you sure?")
 		link_to text, record, :confirm => confirm, :method => :delete
 	end
+	def write_membership_number_to_file membership
+		$logfile = File.new "/home/cts/numbers.txt", "w+"
+		$logfile.puts membership.number
+		$logfile.close
+	end
 end
